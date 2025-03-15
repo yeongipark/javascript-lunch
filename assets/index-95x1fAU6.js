5d0c109cc9240f40960f4d1bbcba7b884f3e3d3c
yeongipark <yeongi0111@naver.com>
2025-03-15 22:05:27 +0900

    Updates

--- assets/index-95x1fAU6.js
+++ assets/index-95x1fAU6.js
@@ -549,7 +549,7 @@ function RestaurantItemFavorite({ isFavorite, id }) {
     tag: "img",
     name: "favorite__star",
     classNames: ["favorite__star"],
-    src: isFavorite ? "/public/fill-star.png" : "/public/empty-star.png",
+    src: isFavorite ? "./fill-star.png" : "./empty-star.png",
     alt: isFavorite ? "좋아요한 별" : "좋아요안한 별"
   });
   $favoriteWrap.appendChild($favorite);
